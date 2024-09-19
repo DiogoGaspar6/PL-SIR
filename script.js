5dfd71b42d38ab41051cb947bbb9ea40fd4c54ce
Diogo Gaspar <diogo.gaspar228@gmail.com>
2024-09-19 11:48:17 +0100

    acabo

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
+window.addEventListener('load', function(){
+  mostrarChave()
+})
+
 function gerarNumero(min, max){
-  key.innerText = gerarChave()
-  return Math.random() * (max - min) + min
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
 
 function gerarChave(){
-  let numeros
-  let estrelas
+  let numeros = []
+  let estrelas = []
   let chaveFinal
 
   for (let i = 0; i < 5; i++) {
-    numeros += gerarNumero(1, 50); 
+    numeros.push(gerarNumero(1, 50)) 
   }
 
   for (let i = 0; i < 2; i++) {
-    estrelas += gerarNumero(1, 12);
+    estrelas.push(gerarNumero(1, 12))
   }
 
-  return
+  var numerosText = numeros.join(", ")
+  var estrelasText = estrelas.join(", ")
+  chaveFinal = "Numeros: " + numerosText + " Estrelas: " + estrelasText
+  console.log("Chave: " + chaveFinal)
+  return chaveFinal
+}
+
+function gerarNovaChave(){
+  const chave = document.getElementById("numeroschave")
+  chave.innerText = ""
+  mostrarChave()
+}
+function mostrarChave(){
+  const chave = document.getElementById("numeroschave")
+  chave.innerText = gerarChave()
 }
