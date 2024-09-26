3921a398616a3221bae3984357a0a2e9f3126ba6
Diogo Gaspar <diogo.gaspar228@gmail.com>
2024-09-26 10:04:28 +0100

    some css style

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
   mostrarChave()
 })
 
-function gerarNumero(min, max){
+function gerarNumero(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
 
-
-function gerarChave(){
-  let numeros = []
-  let estrelas = []
-  let chaveFinal
-
-  for (let i = 0; i < 5; i++) {
-    numeros.push(gerarNumero(1, 50)) 
-  }
-
-  for (let i = 0; i < 2; i++) {
-    estrelas.push(gerarNumero(1, 12))
-  }
-
-  var numerosText = numeros.join(", ")
-  var estrelasText = estrelas.join(", ")
-  chaveFinal = "Numeros: " + numerosText + " Estrelas: " + estrelasText
-  console.log("Chave: " + chaveFinal)
-  return chaveFinal
+function gerarEstrela(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
-function gerarNovaChave(){
-  const chave = document.getElementById("numeroschave")
-  chave.innerText = ""
+function gerarNumeroChave() {
+  let numeros = []
+  let n = 5
+
+  let temp = 0;
+  for (let i = 0; i < n; i++) {
+    temp = gerarNumero(1, 50);
+    while(numeros.includes(temp)){
+      temp = gerarNumero(1, 50);
+    }
+    numeros.push(temp)
+  }
+  var numerosText = numeros.join(", ")
+  return "Numeros: " + numerosText
+}
+
+function gerarEstrelasChave() {
+  let estrelas = []
+  let n = 2
+  let temp = 0
+
+  for (let i = 0; i < n; i++) {
+    temp = gerarEstrela(1,12)
+    while(estrelas.includes(temp)){
+      temp = gerarEstrela(1,12)
+    }
+    estrelas.push(temp)
+  }
+
+  var estrelasText = estrelas.join(", ")
+
+  return "Estrelas: " + estrelasText
+}
+
+function gerarNovaChave() {
   mostrarChave()
 }
-function mostrarChave(){
-  const chave = document.getElementById("numeroschave")
-  chave.innerText = gerarChave()
+function mostrarChave() {
+  const numerochave = document.getElementById("numeroschave")
+  const estrelasChave = document.getElementById("estrelaschave")
+  numerochave.innerText = gerarNumeroChave()
+  estrelasChave.innerText = gerarEstrelasChave()
 }
