f0476d257d35942ee64eb7d8cb95520b7afb90b6
Diogo Gaspar <diogo.gaspar228@gmail.com>
2024-10-03 10:45:12 +0100

    Refactor HTML structure, add favicon, and update dependencies

---
+++ api/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const generateEuromilhoesKey = (req, res) => {
+    let numbers = [];
+    let stars = [];
+
+    for (let i = 0; i < 5; i++) {
+        let number = generateRandomNumber(1, 51);
+
+        while (numbers.includes(number)) {
+            number = generateRandomNumber(1, 51);
+        }
+
+        numbers.push(number);
+    }
+
+    for (let i = 0; i < 2; i++) {
+        let number = generateRandomNumber(1, 13);
+
+        while (stars.includes(number)) {
+            number = generateRandomNumber(1, 13);
+        }
+
+        stars.push(number);
+    }
+
+    return res.status(200).json({
+        "chave": {
+            "numeros": numbers,
+            "estrelas": stars
+        }
+    });
+}
+
+app.use(cors());
+app.get('/generate-key', generateEuromilhoesKey);
+
+const PORT = process.env.PORT || 3004;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = {
+    generateEuromilhoesKey
+}
--- script.js
+++ script.js
@@ -1,76 +1,25 @@
-window.addEventListener('load', function () {
-  mostrarChave()
+const apiURL = "http://localhost:3004/"
+
+window.addEventListener('load', function() {
+    updateHtml();
 })
 
-function gerarNumero(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
+function updateHtml() {
+    let numbers = document.getElementById("numeroschave");
+    let stars = document.getElementById("estrelaschave");
+
+    getEuromilhoesKey().then(json => {
+        numbers.innerHTML = json.key.numbers;
+        stars.innerHTML = json.key.stars;
+    });
 }
 
-function gerarEstrela(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
+async function getEuromilhoesKey() {
+    let response = await fetch(apiURL + "generate-key");
+    let json = await response.json();
+    return json;
 }
 
-function gerarNumeroChave() {
-  let numeros = []
-  let n = 5
-
-  let temp = 0;
-  for (let i = 0; i < n; i++) {
-    temp = gerarNumero(1, 50);
-    while(numeros.includes(temp)){
-      temp = gerarNumero(1, 50);
-    }
-    numeros.push(temp)
-  }
-  return numeros
-}
-
-function gerarEstrelasChave() {
-  let estrelas = []
-  let n = 2
-  let temp = 0
-
-  for (let i = 0; i < n; i++) {
-    temp = gerarEstrela(1,12)
-    while(estrelas.includes(temp)){
-      temp = gerarEstrela(1,12)
-    }
-    estrelas.push(temp)
-  }
-  return estrelas
-}
-
-function gerarNovaChave() {
-  mostrarChave()
-}
-function mostrarChave() {
-  const numerochave = document.getElementById("numeroschave")
-  const estrelasChave = document.getElementById("estrelaschave")
-
-  const numeros = gerarNumeroChave()
-  const estrelas = gerarEstrelasChave()
-
-  numerochave.innerText = "Numeros: " + numeros.join(", ")
-  estrelasChave.innerText = "Estrelas: " + estrelas.join(", ")
-
-  const chave = {
-    numeros: numeros,
-    estrelas: estrelas
-  }
-
-  const chaveJSON = JSON.stringify(chave)
-  console.log(chaveJSON)
-
-  fetch('http://localhost:3000/save', {
-    method: 'POST',
-    headers:{
-      'Content-Type': 'application/json'
-    },
-    body:{
-      body: chaveJSON
-    }
-  })
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error('[ERROR]' + error))
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
