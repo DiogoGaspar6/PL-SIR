0e972d34f37ca1908c5d1d042b21078616cbb20b
DiogoGaspar10 <diogo.g@ipvc.pt>
2024-10-27 09:12:03 +0000

    change the start app

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static(__dirname))
 
 
 const generateRandomNumber = (min, max) => {
